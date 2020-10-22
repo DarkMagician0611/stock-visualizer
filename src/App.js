049b93d5494242d95f333c581f0485ad236e9872
Shubham Chaudhary <shubham.bspdesign@gmail.com>
2020-10-22 22:56:58 +0530

    Added select dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Company } from "./Company";
+import TICKERS from "./data/tickers";
 
 function App() {
+  const tickers = TICKERS;
+  const [company, setCompany] = useState(tickers[0]);
+
+  const handleChange = (event) => {
+    setCompany(event.target.value);
+  };
+
   return (
     <div className="app">
-      <Company company="IBM" />
+      <select onChange={handleChange}>
+        {tickers.map((ticker) => (
+          <option value={ticker}>{ticker}</option>
+        ))}
+      </select>
+      <Company company={company} />
     </div>
   );
 }
